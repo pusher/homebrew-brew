bc96428823c310354a3c892d21c89fd35797db4f
goreleaserbot <kevin.norman@pusher.com>
2017-12-12 15:36:08 +0000

    pusher version v0.11

--- pusher.rb
+++ pusher.rb
@@ -1,9 +1,9 @@
 class Pusher < Formula
   desc "Pusher CLI!"
   homepage "https://pusher.com/"
-  url "https://github.com/pusher/cli/releases/download/v0.10/pusher_0.10_darwin_amd64.zip"
-  version "0.10"
-  sha256 "29083786e53d209bbfee315f0473d027f26dab39a2d307ec354730b26ed0e0c0"
+  url "https://github.com/pusher/cli/releases/download/v0.11/pusher_0.11_darwin_amd64.zip"
+  version "0.11"
+  sha256 "478f8d0a322c2b0428fb6c3647b7ef3e9d85795bab083cae7b43097299489a9d"
 
   def install
     bin.install "pusher"
